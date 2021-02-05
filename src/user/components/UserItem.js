02d68e83cb436694230ce6841ba8e313621fe777
Alejandro Villa <alexandros17.sk@gmail.com>
2021-02-05 14:29:35 -0600

    adding a UserList Page/Component

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './UserItem.css';
+
+const UserItems = () => {
+
+};
+
+export default UserItems;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,20 @@
 import React from "react";
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-    return <h2>Users works!</h2>
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Cheems',
+            image: 'https://m.media-amazon.com/images/I/51nwxb8IVmL._AC_SL1000_.jpg',
+            places: 3
+        },
+    ]
+
+    return (
+        <UsersList items={USERS} />
+    );
 };
 
 export default Users;
