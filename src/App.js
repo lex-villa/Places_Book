7523422da114812b1e2d4f1db5eb5dbc82287a0e
Alejandro Villa <alexandros17.sk@gmail.com>
2021-02-08 14:16:24 -0600

    some fixes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import './App.css';
 
 function App() {
   return (
@@ -13,6 +14,9 @@ function App() {
         <Switch>
           <Route path="/" exact>
             <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
           </Route>
           <Route path="/places/new" exact>
             <NewPlace />
