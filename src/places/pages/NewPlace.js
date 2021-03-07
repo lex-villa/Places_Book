b0a3bebcf53d4a5ce4d80260bee69e52302a4678
Alejandro Villa <alexandros17.sk@gmail.com>
2021-03-06 19:45:08 -0600

    uploading images for new places

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,96 +1,105 @@
-import React, { useContext } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
 
-import Input from '../../shared/components/FormElements/Input';
-import Button from '../../shared/components/FormElements/Button';
-import ErrorModal from '../../shared/components/UIElements/ErrorModal';
-import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
-import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
-import { useForm } from '../../shared/hooks/form-hook';
-import { useHttpClient } from '../../shared/hooks/http-hook';
-import { AuthContext } from '../../shared/context/auth-context';
-import './PlaceForm.css';
-
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import ImageUpload from "../../shared/components/FormElements/ImageUpload";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+import { useForm } from "../../shared/hooks/form-hook";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import { AuthContext } from "../../shared/context/auth-context";
+import "./PlaceForm.css";
 
 const NewPlace = () => {
-    const auth = useContext(AuthContext);
-    const { isLoading, error, sendRequest, clearError } = useHttpClient();
-    const [formState, inputHandler] = useForm({
-        title: {
-            value: '',
-            isValid: false,
-        },
-        description: {
-            value: '',
-            isValid: false,
-        },
-        address: {
-            value: '',
-            isValid: false,
-        },
-    }, false);
+  const auth = useContext(AuthContext);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+      address: {
+        value: "",
+        isValid: false,
+      },
+      image: {
+        value: null,
+        isValid: false,
+      },
+    },
+    false
+  );
 
-    const history = useHistory();
+  const history = useHistory();
 
-    const placeSubmitHandler = async event => {
-        event.preventDefault();
-        try {
-            await sendRequest('http://localhost:5000/api/places',
-                'POST',
-                JSON.stringify({
-                    title: formState.inputs.title.value,
-                    description: formState.inputs.description.value,
-                    address: formState.inputs.address.value,
-                    creator: auth.userId
-                }),
-                { 'Content-Type': 'application/json' }
-            );
-            history.push('/');
-        } catch (error) {
+  const placeSubmitHandler = async (event) => {
+    event.preventDefault();
+    try {
+      const formData = new FormData();
+      formData.append("title", formState.inputs.title.value);
+      formData.append("description", formState.inputs.description.value);
+      formData.append("address", formState.inputs.address.value);
+      formData.append("creator", auth.userId);
+      formData.append("image", formState.inputs.image.value);
+      await sendRequest("http://localhost:5000/api/places", "POST", formData);
+      history.push("/");
+    } catch (error) {}
+  };
 
-        };
-    };
+  return (
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      <form className="place-form" onSubmit={placeSubmitHandler}>
+        {isLoading && <LoadingSpinner asOverlay />}
+        <Input
+          id="title"
+          element="input"
+          type="text"
+          label="Title"
+          errorText="Please enter a valid title."
+          validators={[VALIDATOR_REQUIRE()]}
+          onInput={inputHandler}
+        />
 
-    return (
-        <>
-            <ErrorModal error={error} onClear={clearError} />
-            <form className="place-form" onSubmit={placeSubmitHandler}>
-                {isLoading && <LoadingSpinner asOverlay />}
-                <Input
-                    id="title"
-                    element="input"
-                    type="text"
-                    label="Title"
-                    errorText="Please enter a valid title."
-                    validators={[VALIDATOR_REQUIRE()]}
-                    onInput={inputHandler}
-                />
+        <Input
+          id="description"
+          element="textarea"
+          label="Description"
+          errorText="Please enter a valid description (at least 5 characters)."
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          onInput={inputHandler}
+        />
 
-                <Input
-                    id="description"
-                    element="textarea"
-                    label="Description"
-                    errorText="Please enter a valid description (at least 5 characters)."
-                    validators={[VALIDATOR_MINLENGTH(5)]}
-                    onInput={inputHandler}
-                />
-
-                <Input
-                    id="address"
-                    element="input"
-                    type="text"
-                    label="Address"
-                    errorText="Please enter a valid address."
-                    validators={[VALIDATOR_REQUIRE()]}
-                    onInput={inputHandler}
-                />
-
-                <Button type="submit" disabled={!formState.isValid}>
-                    ADD PLACE
-                </Button>
-            </form>
-        </>
-    );
+        <Input
+          id="address"
+          element="input"
+          type="text"
+          label="Address"
+          errorText="Please enter a valid address."
+          validators={[VALIDATOR_REQUIRE()]}
+          onInput={inputHandler}
+        />
+        <ImageUpload
+          id="image"
+          onInput={inputHandler}
+          errorText="Please provide an image."
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          ADD PLACE
+        </Button>
+      </form>
+    </>
+  );
 };
 
 export default NewPlace;
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -127,7 +127,12 @@ const Auth = () => {
           )}
 
           {!isLoginMode && (
-            <ImageUpload center id="image" onInput={inputHandler} />
+            <ImageUpload
+              center
+              id="image"
+              onInput={inputHandler}
+              errorText="Please provide an image."
+            />
           )}
 
           <Input
